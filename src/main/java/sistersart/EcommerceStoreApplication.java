e19a1c92f01cbbce0cd6952f31b64d469d9a9e9d
zlatsz <zlatsz@abv.bg>
2021-10-17 10:48:14 +0300

    Set up Spring

--- src/main/java/sistersart/EcommerceStoreApplication.java
+++ src/main/java/sistersart/EcommerceStoreApplication.java
@@ -1,4 +1,4 @@
-package com.sistersart.ecommercestore;
+package sistersart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/sistersart/ecommercestore/EcommerceStoreApplicationTests.java
+++ src/test/java/sistersart/ecommercestore/EcommerceStoreApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sistersart.ecommercestore;
+package sistersart.ecommercestore;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
